c0dbf04ab3ae27dc315f15bcb45bcfacbdd7e880
Charles Griffin <cgriffin@indatus.com>
2015-04-25 20:39:13 -0400

    functions and variable example

--- src/talk_clojure_to_me/core.clj
+++ src/talk_clojure_to_me/core.clj
@@ -2,15 +2,17 @@
   (:gen-class))
 
 ;;;;Let's play around with the REPL a bit
-;;;;Conditionals
+;;;;Conditionals with function
 (defn condy
   [x]
   (if (= x 2)
-    (println "two")
-    (println "sorry dude, you're not two")))
+    "two"
+    "sorry dude, you're not two"))
 
 
 (defn -main
   "Talk Clojure to me"
   [& args]
-  (condy 2))
+  ;;storing variables
+  (def store-condy (condy 2))
+  (println store-condy))
