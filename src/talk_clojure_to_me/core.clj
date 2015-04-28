00992a1830ae40d9ce77d63cd7a0480923fe07be
Charles Griffin <cgriffin@indatus.com>
2015-04-28 10:57:38 -0400

    working on lists and vectors

--- src/talk_clojure_to_me/core.clj
+++ src/talk_clojure_to_me/core.clj
@@ -1,25 +1,15 @@
 (ns talk-clojure-to-me.core
   (:gen-class))
 
-;;;;Let's play around with the REPL a bit
-;;;;Conditionals
-(defn condy
-  [x]
-  (if (= x 2)
-    (println "two")
-    (println "sorry dude, you're not two")))
-
-;;;vectors
-
-;;;store a vector in a variable
-(def my-list [1 2 3])
-
-;;;add an element to a vector
-
-
-
 
 (defn -main
   "Talk Clojure to me"
   [& args]
-  (println my-list))
+
+  (def ima-list '(1 2 3))
+
+  (def my-list [1 2 3])
+
+  (println ima-list)
+
+  )
