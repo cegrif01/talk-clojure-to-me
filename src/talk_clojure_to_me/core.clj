41a44be4bb370327b5d1def5e8b27206cffd23f4
Charles Griffin <cgriffin@indatus.com>
2015-04-27 23:23:53 -0400

    added a few more functions

--- src/talk_clojure_to_me/core.clj
+++ src/talk_clojure_to_me/core.clj
@@ -13,6 +13,17 @@
   [some-list-or-vector]
   (apply * some-list-or-vector))
 
+;;this function combines a few concepts.  Let's break
+;;this one down
+(defn let-there-be-magic
+  [vec1 vec2]
+  (let [doubled-vec    (double-everything vec1)
+        multiplied-vec (multiply-together vec2)]
+
+    (if (< (reduce + doubled-vec) multiplied-vec)
+      vec1
+      vec2)))
+
 
 (defn -main
   "Talk Clojure to me"
@@ -26,4 +37,27 @@
   ;;calling map on more than one element will combine them
   (println (map str ["a" "b" "c"] ["A" "B" "C"] ["d" "e" "z"]))
 
+  ;;prepends 1 to the beginning of the list
+  (println (cons 1 '(2 3 4)))
+
+
+  ;;Returns a new seq where x is the first element and seq is the rest.
+  ;;notice that the first item is not expanded
+  (println (cons [2 3 4] [1 2]))
+
+  ;;conj will add an integer to the end
+  (println (conj [1 2 3] 2))
+
+  ;;conj will add an integer to end of a vector
+  (println (conj [1 2 3] 2))
+
+  ;;conj will add an integer to beginning of a list
+  (println (conj '(1 2 3) 2))
+
+  ;;will return a new list with all elements except the first
+  (println (rest '(3 5 7)))
+
+  ;;let allows you to store local variables
+   (println (let-there-be-magic [2 4 6] [3 4 5]))
+;;  (let-there-be-magic [2 4 6] [3 4 5])
   )
